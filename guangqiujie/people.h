b1ae9449437571c56ab9ad74c5afc6f05129957c
刘兵 <liubing@liubingdeimac.local>
2018-12-11 14:46:22 +0800

    提交测试一下

---
+++ guangqiujie/people.h
@@ -0,0 +1,17 @@
+//
+//  people.h
+//  guangqiujie
+//
+//  Created by 刘兵 on 2018/12/11.
+//  Copyright © 2018年 刘兵. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface people : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ guangqiujie/people.m
@@ -0,0 +1,13 @@
+//
+//  people.m
+//  guangqiujie
+//
+//  Created by 刘兵 on 2018/12/11.
+//  Copyright © 2018年 刘兵. All rights reserved.
+//
+
+#import "people.h"
+
+@implementation people
+
+@end
