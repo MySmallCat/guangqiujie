37d7bc013c85e3ae964c438b445e1b35765ca92e
刘兵 <liubing@liubingdeimac.local>
2018-12-11 14:06:41 +0800

    Initial Commit

---
+++ guangqiujie/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  guangqiujie
+//
+//  Created by 刘兵 on 2018/12/11.
+//  Copyright © 2018年 刘兵. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ guangqiujie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  guangqiujie
+//
+//  Created by 刘兵 on 2018/12/11.
+//  Copyright © 2018年 刘兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
